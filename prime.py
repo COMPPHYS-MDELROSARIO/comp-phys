3c55e562561d034d7218cbe7783a430c4aeec094
COMPPHYS-MDELROSARIO <mjdelrosario@usfca.edu>
2015-09-23 18:54:40 -0700

    finished prime

---
+++ prime.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def prime(a, b):
+    prime_range = np.arange(a,b)
+
+    for num in prime_range:
+        prime = True
+        for i in range(2,num):
+            if (num%i==0):
+                prime = False
+        if prime:
+           print num
+
+a, b = input("Please enter a range of numbers (a,b):" )
+prime(a,b)
