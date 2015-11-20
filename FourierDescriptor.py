9a890be6ba54e7ef94dd6f97677e446b65d21e92
COMPPHYS-MDELROSARIO <mjdelrosario@usfca.edu>
2015-11-19 20:00:23 -0800

    final commit

--- FourierDescriptor.py
+++ FourierDescriptor.py
@@ -40,11 +40,11 @@ def extract_shape (im_file, blowup = 1.0, plot_img = False, plot_contour = False
     
     print 'number of contour path', len(cs_paths)
     
-    for x in range(len(cs_paths)):
+    for x in range(0, len(cs_paths)):
         p = cs_paths[x]
         v = p.vertices 
-        xray = (v[:,0])
-        yray = (v[:,1])
+        xray.append(v[:,0])
+        yray.append(v[:,1])
         plt.figure(0)
         plt.plot(xray[x], yray[x])
         plt.savefig("rec_numbers126.pdf")
@@ -54,7 +54,7 @@ def extract_shape (im_file, blowup = 1.0, plot_img = False, plot_contour = False
         plt.close()
     if plot_contour_pts:
         plt.figure(0)
-        for y in range(len(xray)):
+        for y in range(0, len(xray)):
             plt.scatter(xray[x], yray[x]) 
         
     return xray, yray
@@ -93,7 +93,7 @@ def get_FD_abs(x, y, order = 10, norm = True, no_zeroth = True):
     FD_mag = []
     yr = []
     xr = []
-    for b in range(len(x)):
+    for b in range(0, len(x)):
         z = FD(x[b], y[b])
         z_filt = filt_FD(z, order, no_zeroth = True, norm = True)
         if norm:
@@ -117,23 +117,19 @@ def recover_shape(Z):
 def size_norm(Z):
     return Z/np.sqrt(np.abs(Z[1]) * np.abs(Z[-1]))
 
-norm=True
-order = 10
-no_zeroth = False
-
-parser = argparse.ArgumentParser(description = 'checks for norm and zeroth')
-parser.add_argument('-norm', dest='norm', action='store_false')
-parser.add_argument('-no_zeroth', dest = 'no_zeroth', action='store_false')
-parser.set_defaults(no_zeroth=True, norm=True)
-args = parser.parse_args()
+#parser = argparse.ArgumentParser(description = 'checks for norm and zeroth')
+#parser.add_argument('-norm', dest='norm', action='store_false')
+#parser.add_argument('-no_zeroth', dest = 'no_zeroth', action='store_false')
+#parser.set_defaults(no_zeroth=True, norm=True)
+#args = parser.parse_args()
 
 a, b = extract_shape('number1.png', blowup = 1.0, plot_img = False, plot_contour = False, plot_contour_pts = False)
 c, d = extract_shape('number2.png', blowup = 1.0, plot_img = False, plot_contour = False, plot_contour_pts = False)
 e, f = extract_shape('number6.png', blowup = 1.0, plot_img = False, plot_contour = False, plot_contour_pts = False)
 
-#fd1, x_rec1, y_rec1 = get_FD_abs(a, b, order = order, norm = norm, no_zeroth=no_zeroth)
-#fd2, x_rec2, y_rec2 = get_FD_abs(c, d, order = order, norm = norm, no_zeroth=no_zeroth)
-#fd3, x_rec3, y_rec3 = get_FD_abs(e, f, order = order, norm = norm, no_zeroth=no_zeroth)
+#fd1, x_rec1, y_rec1 = get_FD_abs(a, b, order = 10, norm = True, no_zeroth = False)
+#fd2, x_rec2, y_rec2 = get_FD_abs(c, d, order = 10, norm = True, no_zeroth = False)
+#fd3, x_rec3, y_rec3 = get_FD_abs(e, f, order = 10, norm = True, no_zeroth = nFalse)
 
 plt.figure()
 plt.show()
